39384d24380a17b19ee2dab150f896173d8ae3d1
Viky Jaware <97377174+vikyjaware@users.noreply.github.com>
2022-02-13 00:35:23 +0530

    Update UnitTest1.cs

--- MaxNumberTest/UnitTest1.cs
+++ MaxNumberTest/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace MaxNumberTest
         public void GivenFloatValuesFistPositionMax_WhenCheck__ShouldReturnMaxNumber()
         {
             TestMaximum maximum = new TestMaximum();
-            double max = maximum.MaximumIntNumberCheck(3.3, 2.2, 1.1);
+            double max = maximum.MaximumFloatNumberCheck(3.3, 2.2, 1.1);
             Assert.AreEqual(3.3, max);
         }
 
@@ -17,14 +17,14 @@ namespace MaxNumberTest
         public void GivenFloatValuesSecondPositionMax_WhenCheck_ShouldReturnMaxNumber()
         {
             TestMaximum maximum = new TestMaximum();
-            double max = maximum.MaximumIntNumberCheck(1.1, 3.3, 2.2);
+            double max = maximum.MaximumFloatNumberCheck(1.1, 3.3, 2.2);
             Assert.AreEqual(3.3, max);
         }
         [Test]
         public void GivenFloatValuesThirdPositionMax_WhenCheck_ShouldReturnMaxNumber()
         {
             TestMaximum maximum = new TestMaximum();
-            double max = maximum.MaximumIntNumberCheck(1.1, 2.2, 3.3);
+            double max = maximum.MaximumFloatNumberCheck(1.1, 2.2, 3.3);
             Assert.AreEqual(3.3, max);
         }
     }
