a24da1fbdcd9af3f6cc8ee1481206f497eb975c1
vikyjaware <vikyjaware@gmail.com>
2022-02-12 17:46:50 +0530

    [Viky] Add : Folder structure added

--- MaxNumberTest/UnitTest1.cs
+++ MaxNumberTest/UnitTest1.cs
@@ -6,7 +6,7 @@ namespace MaxNumberTest
     public class Tests
     {
         [Test]
-        public void GivenFloatValuesFistPositionMax_WhenCheck__ShouldReturnMaxNum()
+        public void GivenFloatValuesFistPositionMax_WhenCheck__ShouldReturnMaxNumber()
         {
             TestMaximum maximum = new TestMaximum();
             double max = maximum.MaximumFloatNumberCheck(3.3, 2.2, 1.1);
@@ -14,14 +14,14 @@ namespace MaxNumberTest
         }
 
         [Test]
-        public void GivenFloatValuesSecondPositionMax_WhenCheck_ShouldReturnMaxNum()
+        public void GivenFloatValuesSecondPositionMax_WhenCheck_ShouldReturnMaxNumber()
         {
             TestMaximum maximum = new TestMaximum();
             double max = maximum.MaximumFloatNumberCheck(1.1, 3.3, 2.2);
             Assert.AreEqual(3.3, max);
         }
         [Test]
-        public void GivenFloatValuesThirdPositionMax_WhenCheck_ShouldReturnMaxNum()
+        public void GivenFloatValuesThirdPositionMax_WhenCheck_ShouldReturnMaxNumber()
         {
             TestMaximum maximum = new TestMaximum();
             double max = maximum.MaximumFloatNumberCheck(1.1, 2.2, 3.3);
