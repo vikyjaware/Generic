311ccb568aa1f7b32e19a4413d7056217ee196ed
Viky Jaware <97377174+vikyjaware@users.noreply.github.com>
2022-02-13 01:05:23 +0530

    Update UnitTest1.cs

--- MaxNumberTest/UnitTest1.cs
+++ MaxNumberTest/UnitTest1.cs
@@ -9,77 +9,24 @@ namespace MaxNumberTest
         TestMaximum<double> maxFloat = new TestMaximum<double>();
         TestMaximum<string> maxString = new TestMaximum<string>();
       //For integers
-        [Test]
-<<<<<<< HEAD
-        public void GivenFloatValuesFistPositionMax_WhenCheck__ShouldReturnMaxNumber()
+       [Test]
+        public void GivenIntegerValuesFistPositionMax_WhenCheck__ShouldReturnMaxNum()
         {
-<<<<<<< HEAD
-            TestMaximum maximum = new TestMaximum();
-<<<<<<< HEAD
-            double max = maximum.MaximumFloatNumberCheck(3.3, 2.2, 1.1);
-=======
-            double max = maximum.MaximumIntNumberCheck(3.3, 2.2, 1.1);
->>>>>>> UC2-FloatMaximumNumber
-            Assert.AreEqual(3.3, max);
-=======
             int max = maxInt.MaximumCheck(3, 2, 1);
             Assert.AreEqual(3, max);
->>>>>>> UC4-RefactorUsingGeneric
         }
 
         [Test]
-        public void GivenFloatValuesSecondPositionMax_WhenCheck_ShouldReturnMaxNumber()
+        public void GivenIntegerValuesSecondPositionMax_WhenCheck_ShouldReturnMaxNum()
         {
-<<<<<<< HEAD
-            TestMaximum maximum = new TestMaximum();
-<<<<<<< HEAD
-            double max = maximum.MaximumFloatNumberCheck(1.1, 3.3, 2.2);
-=======
-            double max = maximum.MaximumIntNumberCheck(1.1, 3.3, 2.2);
->>>>>>> UC2-FloatMaximumNumber
-            Assert.AreEqual(3.3, max);
-=======
             int max = maxInt.MaximumCheck(1, 3, 2);
             Assert.AreEqual(3, max);
->>>>>>> UC4-RefactorUsingGeneric
         }
         [Test]
-        public void GivenFloatValuesThirdPositionMax_WhenCheck_ShouldReturnMaxNumber()
+        public void GivenIntegerValuesThirdPositionMax_WhenCheck_ShouldReturnMaxNum()
         {
-<<<<<<< HEAD
-            TestMaximum maximum = new TestMaximum();
-<<<<<<< HEAD
-            double max = maximum.MaximumFloatNumberCheck(1.1, 2.2, 3.3);
-=======
-            double max = maximum.MaximumIntNumberCheck(1.1, 2.2, 3.3);
->>>>>>> UC2-FloatMaximumNumber
-            Assert.AreEqual(3.3, max);
-=======
-        public void GivenStringValuesFistPositionMax_WhenCheck__ShouldReturnMaxNum()
-        {
-            TestMaximum maximum = new TestMaximum();
-            string max = maximum.MaximumCheck("Peach", "Apple", "Banana");
-            Assert.AreEqual("Peach", max);
-        }
-
-        [Test]
-        public void GivenStringValuesSecondPositionMax_WhenCheck_ShouldReturnMaxNum()
-        {
-            TestMaximum maximum = new TestMaximum();
-            string max = maximum.MaximumCheck("Banana", "Peach", "Apple");
-            Assert.AreEqual("Peach", max);
-        }
-        [Test]
-        public void GivenStringValuesThirdPositionMax_WhenCheck_ShouldReturnMaxNum()
-        {
-            TestMaximum maximum = new TestMaximum();
-            string max = maximum.MaximumCheck("Apple", "Banana", "Peach");
-            Assert.AreEqual("Peach", max);
->>>>>>> UC3-StringMaximum
-=======
             int max = maxInt.MaximumCheck(1, 2, 3);
             Assert.AreEqual(3, max);
->>>>>>> UC4-RefactorUsingGeneric
         }
 
         //For Float
